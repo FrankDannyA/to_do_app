5ad14f3c0fb5a1b7a28329d8091d3e16c217ad68
Daniil Frank <orgfrank@icloud.com>
2023-02-12 17:46:36 +0600

    ToDo decoration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/utils/todo_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //list of todo tasks
+  List toDoList = [
+    ['Do Anything', true],
+    ['Do a to do', false]
+  ];
+
+  //checbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +30,15 @@ class HomePage extends StatelessWidget {
         title: const Text('TO DO'),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          TodoTile(
-            isComleted: true,
-            taskName: 'Create To Do app',
-            onChanged: (p0) {},
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            isComleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -15,22 +15,33 @@ class TodoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.only(left: 16, top: 20, right: 16),
       child: Container(
-        padding: EdgeInsets.all(24),
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
         child: Row(
           children: [
             //checkbox
             Checkbox(
               value: isComleted,
               onChanged: onChanged,
+              activeColor: Colors.black,
             ),
 
             //name task
-            Text(taskName),
+            Text(
+              taskName,
+              style: TextStyle(
+                decoration: isComleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
           ],
         ),
-        decoration: BoxDecoration(color: Colors.yellow),
       ),
     );
   }
